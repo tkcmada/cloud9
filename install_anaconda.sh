5afc3813ee5e2b83bf71d8f6f86834139f2f5806
EC2 Default User <ec2-user@ip-10-0-1-163.ec2.internal>
2018-06-04 05:20:16 +0000

    added install pyenv and anaconda scripts

---
+++ install_anaconda.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+pyenv install anaconda3-5.0.1
+pyenv rehash
+pyenv versions
---
+++ install_pyenv.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+source ~/.bashrc
